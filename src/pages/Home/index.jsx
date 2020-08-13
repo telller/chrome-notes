8f9a55c178614a72ac331f95dd1e361963705cc3
Vlad Golyachenko <vgolyachenko@deloitte.com>
2020-08-14 01:30:00 +0300

    minor

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef } from 'react'
 import ReactQuill from 'react-quill'
 import './counter'
 import 'react-quill/dist/quill.snow.css'
